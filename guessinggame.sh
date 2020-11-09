d34347bff98d65ce8b5162cba02ec085986c56fe
moeedt90 <moeedtariq279@yahoo.com>
2020-11-09 23:49:09 +0500

    new logic incorporated

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,29 +5,32 @@ User_Input_count=0
 Actual_File_count=$(ls | wc -l)
 function User_Input {
 
-	        echo "Enter the guess for number of files in the current directory"
-		        read User_Input_count
+	echo "Enter the guess for number of files in the current directory"
+	read User_Input_count
 			        }
 
 			while true
 			do
-				        User_Input
-					        if [[ $User_Input_count == +([0-9]) ]]
-							        then
+				User_Input
 
-									                if [[ $User_Input_count -lt $Actual_File_count  ]]
-												                then
-															                        echo "Your guessed file count is too low"
-																		                elif [[ $User_Input_count -gt $Actual_File_count ]]
-																					                then
-																								                        echo "Your guessed file count is too high"
-																											                else
-																														                        echo " congratulation"
-																																	                        break;
-																																				                fi
-																																						        else
+				if [[ $User_Input_count == +([0-9]) ]]
 
-																																								                echo "$User_Input_count is not a number"
-																																										        fi
-																																										done
+				then
+					if [[ $User_Input_count -lt $Actual_File_count  ]]
+
+					then
+						echo "Your guessed file count is too low"
+
+					elif [[ $User_Input_count -gt $Actual_File_count ]]
+
+					then
+						echo "Your guessed file count is too high"
+					else
+						echo " congratulation"
+						break;
+					fi
+				else
+					echo "$User_Input_count is not a number"
+				fi
+			done
 			
