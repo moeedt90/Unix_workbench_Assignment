364276f0200809af848c3c097eaca1daa308a393
moeedt90 <moeedtariq279@yahoo.com>
2020-11-09 14:56:24 +0500

    new file updated

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 #File guessingame.sh
 
-file_count=0
-function read_guess {
+count=0
+
+function guess {
 echo "How many files are in the current directory:"
-read file_count
+read count
 }
 
 while true
 do
-	read_guess
-	if [[ $file_count == ?(-)+([0-9]) ]]
+	guess
+	if [[ $count == ?(-)+([0-9]) ]]
 	then 
            actual_file_count=`ls -al | egrep ^[-] | wc -l`
-         if [[ $actual_file_count -eq $file_count ]]
+         if [[ $actual_file_count -eq $count ]]
 	 then
 		 echo "Congratulation, It is correct!!"
 		 break;
-	 elif [[ $file_count -lt $actual_file_count  ]]
+	 elif [[ $count -lt $actual_file_count  ]]
 	 then
 		 echo "Guessed file count is too low"
-	 elif [[ $file_count -gt $actual_file_count ]]
+	 elif [[ $count -gt $actual_file_count ]]
 	 then
 		 echo "Guessed file count is too high"
 		fi
